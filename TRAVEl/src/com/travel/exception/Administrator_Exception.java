aa894d5cc8d9e4f065640f6b72197c078c255795
RIYAS <riyass973@gmail.com>
2022-11-09 23:03:31 +0530

    day2

---
+++ TRAVEl/src/com/travel/exception/Administrator_Exception.java
@@ -0,0 +1,14 @@
+package com.travel.exception;
+
+public class Administrator_Exception extends Exception{
+	
+	public Administrator_Exception() {
+		
+	}
+	
+	public Administrator_Exception(String msg) {
+		
+		super(msg);
+	}
+
+}
---
+++ TRAVEl/src/com/travel/exception/hola.java
@@ -0,0 +1,10 @@
+package com.travel.exception;
+
+public class hola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("lop");
+	}
+
+}
